cd84ad43e6759c3201dbb53045652623c4b81b20
olga <bachisheo@gmail.com>
2022-04-09 10:20:29 +0700

    Endpoint for main page was created

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/express/templates/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
+app.get('/all', (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/templates/all_products.html'));
+})
