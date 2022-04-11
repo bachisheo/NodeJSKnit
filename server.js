7539911fe1d24d2553a991a3bb3528cc9768cf8b
olga <bachisheo@gmail.com>
2022-04-11 10:43:15 +0700

    Routing for "products" is placed in a separate file

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
-const {sequelize} = require('./models/product');
 const app = express();
 const db = require("./models/index");
 const products = db.products;
 
 app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/templates/index.html'));
-    //__dirname : It will resolve to your project folder.
-});
+
 const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
 
+//routes
+app.use('/products/', require('./routes/products'));
+app.get('/pr', (req, res) => res.send("aaaa"));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/express/templates/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
 ///Get main page
 app.get('/all', (req, res) =>{
     products.findOne().then(function (user) {
