4b6af8b23f0c20fbc649646489a10a575ce22fd6
olga <bachisheo@gmail.com>
2022-04-11 16:37:34 +0700

    quick and dirty resolve a " it is not an "own property" of its parent" handlebars problem  (https://stackoverflow.com/questions/59690923/handlebars-access-has-been-denied-to-resolve-the-property-from-because-it-is)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const db = require("./config/database");
 const {engine} = require('express-handlebars');
 const port = 3000;
 const path = require('path');
+const Handlebars = require('Handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
 const app = express();
 app.use(express.json());
@@ -13,9 +15,14 @@ server.listen(port);
 console.debug('Server listening on port ' + port);
 
 //Middleware for handlebars
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    // ...implement newly added insecure prototype access
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+
 app.set('view engine', 'handlebars');
-app.set('defaultLayout', 'main');
+
 app.use(express.static(path.join(__dirname,'public')));
 
 //routes
